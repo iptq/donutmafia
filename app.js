e12e50a0b112caf8ab241644da5d8cd06d9b8d32
Michael Zhang <failed.down@gmail.com>
2015-02-03 13:43:58 -0600

    yolo

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var http = require("http");
+// var api = require("./api/api");
+// var router = require("./app/router");
+require("dotenv").load();
+
+var app = express();
+
+app.configure(function() {
+	app.set("port", process.env.PORT || 5000);
+	app.set("views", __dirname + "/app/jade");
+	app.set("view engine", "jade");
+	app.locals.pretty = true;
+
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.methodOverride());
+	app.use(express.session({
+		// secret: process.env.APP_SECRET
+		secret: "no one gives a shit"
+	}));
+
+	app.use(express.static(__dirname + "/web"));
+});
+
+// api(app);
+// router(app);
+
+http.createServer(app).listen(app.get("port"), function() {
+	console.log("[app.js] listening on port " + app.get("port") + "...");
+});
